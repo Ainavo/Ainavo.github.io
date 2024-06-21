8c6682168a01fa5d1a8369ee62765ecbcc362baa
“Ainavo” <Ainavo@163.com>
2024-06-21 23:03:08 +0800

    Site updated: 2024-06-21 23:03:02

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1718981744330";
+const VERSION = "1718982108729";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
