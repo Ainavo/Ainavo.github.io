f3902765ec4e496c770e3f0d32bfa9fe0b4ef236
ainavo <ainavo@163.com>
2024-08-26 09:38:08 +0800

    Site updated: 2024-08-26 09:37:50

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1724635156056";
+const VERSION = "1724636245836";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
